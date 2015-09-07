5ad31ac815de2bbcc9c719974eec2041f44d5fdc
wsedor <wsedor@wpi.edu>
2015-09-07 00:31:09 -0400

    Robot.py class
    Added Robot.py class

---
+++ Project_1/Robot.py
@@ -0,0 +1,15 @@
+import Node
+
+class Robot:
+    def _init_(dir,curNode):
+        self.dir = dir
+        self.curNode = curNode
+
+    def fwd():
+
+    def bash():
+
+    def turn():
+
+    def boom():
+
