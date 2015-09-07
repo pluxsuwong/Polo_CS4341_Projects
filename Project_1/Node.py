6b41e0baf2edd1754350fc7554eb391ee0e6e5f7
spirasmepulkul <spirasmepulkul@wpi.edu>
2015-09-07 13:09:43 -0400

    edited nextNode function

--- Project_1/Node.py
+++ Project_1/Node.py
@@ -17,22 +17,28 @@ class Node:
 
     # This function takes in the direction, and returns the node according to the orientation
     def nextNode(self, direction):
-        if direction == 'NORTH':
-	#returns the node to the north of the current node (12'oclock)
-            return row+1
+        if direction == 'N':
+	    #returns the node to the north of the current node (12'oclock)
+            #access the map 
+            self.row = row + 1
+            return movableNeighbor[0]
 
-        elif direction == 'EAST':
-        #returns the node to the east of the current node (3'oclock)
-            return col+1
+        elif direction == 'E':
+            #returns the node to the east of the current node (3'oclock)
+            self.col = col + 1
+            return movableNeighbor[1]
 
+        elif direction == 'S':
+            #returns the node to the south of the current node (6'oclock)
+            self.row = row - 1
+            return movableNeighbor[2]
 
-        elif direction == 'SOUTH':
-        #returns the node to the south of the current node (6'oclock)
-            return row-1
+        elif direction == 'W':
+            #returns the node to the west of the current node (9'oclock)
+            self.col = col - 1
+            return movableNeighbor[3]
 
-        else:
-        #returns the node to the west of the current node (9'oclock)
-            return col-1
-
-       # print("Hello")
+        else: 
+            #any other cases
+            print 'invalid direction'
 
