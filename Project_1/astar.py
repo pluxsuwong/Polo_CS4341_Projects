fe899d6638dcefeaee94198a4c074611a36d54e9
Peerapat Luxsuwong <pluxsuwong@wpi.edu>
2015-09-07 17:17:02 -0400

    added initial file processing

--- Project_1/astar.py
+++ Project_1/astar.py
@@ -1,15 +1,21 @@
 import fileinput
-
-fd = []
-for line in fileinput.input(args):
-    fd.append(line)
-
-def makeTerrain(fd):
-    newArray = []
-    for line in fd:
-        
-    return newArray
+import re
 
 
+fd = [line for line in fileinput.input()]
 
 
+terrain = []    # 2-D list of lists
+for line in fd:
+    raw_row = re.split(r'\t+', line.rstrip('\t\r\n'))
+    new_row = []
+    for c in raw_row:
+        try:
+            new_row.append(int(c))
+        except ValueError:
+            new_row.append(c)
+    terrain.append(new_row)
+
+
+
+print terrain
