885844ab481438427b446f4c7ef671366634824e
Peerapat Luxsuwong <pluxsuwong@wpi.edu>
2015-09-07 00:50:08 -0400

    made some changes

---
+++ Project_1/astar.py
@@ -0,0 +1,13 @@
+import fileinput
+
+fd = []
+for line in fileinput.input(args):
+    fd.append(line)
+
+def makeTerrain(fd):
+    newArray = []
+    for line in fd:
+        
+    return newArray
+
+
