be140a6d2a85b76bb591b6f3dc765a9d38fa093c
Peerapat Luxsuwong <pluxsuwong@wpi.edu>
2015-09-22 15:08:42 -0400

    implemented random pop init

--- Project_2/ga.py
+++ Project_2/ga.py
@@ -1,9 +1,49 @@
-import fileinput
+import fileinput as fi
 import sys
+import random as rand
 
 # ==== Functions ====
 
 # Genererate initial population
+def gen_init_pop(puzzle, fd, pop_size):
+    # Gene pool
+    GP = []
+    for i in fd:
+        GP.append(i)
+
+    # Population of strings
+    population = []
+    # Generate random strings
+    for i in range(0, pop_size):
+        string = rand_string(puzzle, GP)
+        population.append(string)
+
+    return population
+
+def rand_string(puzzle, GP):
+    chars = GP
+    string = []
+    string_len = -1
+
+    # Puzzle 1
+    if puzzle == 1:
+        string_len = rand.randint(1, len(chars))
+    # Puzzle 2
+    elif puzzle == 2:
+        string_len = 30
+    # Puzzle 3
+    elif puzzle == 3:
+        string_len = rand.randint(1, len(chars))
+    else:
+        print "Error: Invalid Puzzle Number"
+    
+    for i in range(0, string_len):
+        c = rand.choice(chars)
+        string.append(c)
+        chars.remove(c)
+
+    print "Generated: " + string
+    return string
 
 # Evaluate fitness of strings
 
@@ -16,9 +56,15 @@ import sys
 # ==== Format Input ====
 
 puzzle_num = int(sys.argv[1])
-fd = [line for line in fileinput.input(sys.argv[2])]
-fileinput.close()
+fd = [line for line in fi.input(sys.argv[2])]
+fi.close()
 run_time = int(sys.argv[3])
+
+# Only for puzzle 1
+T = 0
+if puzzle_num == 1:
+    T = int(fd[0])
+    del fd[0]
 
 # ==== Run Genetic Algorithm ====
 
