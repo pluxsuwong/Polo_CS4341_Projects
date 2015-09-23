2619fc73333c70297ddba02cdff7f43bdf72ef92
Peerapat Luxsuwong <pluxsuwong@wpi.edu>
2015-09-22 21:35:34 -0400

    broken sorting and shit

--- Project_2/ga.py
+++ Project_2/ga.py
@@ -1,5 +1,7 @@
 import fileinput as fi
 import random as rand
+from operator import itemgetter
+import math
 import sys
 import time 
 
@@ -25,10 +27,10 @@ def gen_init_pop(puzzle, fd, pop_size):
 
 # Generate random string
 def rand_string(puzzle, GP):
-    chars = GP
+    chars = [e for e in GP]
     string = []
     string_len = -1
-
+    
     # Puzzle 1
     if puzzle == 1:
         string_len = rand.randint(1, len(chars))
@@ -70,13 +72,59 @@ def rand_string(puzzle, GP):
         string.append(lookout)
     # Invalid puzzle
     else:
-        print "Error: Invalid Puzzle Number"
+        print "Error: In rand_string() - Invalid Puzzle Number"
     
     return string
 
 # ==== Genetic Algorithm ====
 
+# Fitness functions
+def fitness_function(puzzle, target, population):
+    raw_pop = []
+    total_value = 0.0
+
+    # Puzzle 1
+    if puzzle == 1:
+        for element in population:
+        # Calculate fitness value of element
+            fitness_val = 0
+            diff = target - float(sum(element))
+            if diff <= 0:
+                fitness_val = 0
+            else:
+                fitness_val = -1.0*((diff/target)**2)*100.0 + 100.0
+            
+            total_value += fitness_val
+            element_tup = [fitness_val, element]
+
+            # Add tuple (e, value) to ordered_pop
+            raw_pop.append(element_tup)
+    # Puzzle 2
+    elif puzzle == 2:
+        print '2'
+    # Puzzle 3
+    elif puzzle == 3:
+        print '3'
+    else: 
+        print "Error: In fitness_function() - Invalid Puzzle Number"
+
+    # Normalize element fitness values
+    for i in range(0, len(raw_pop)):
+        raw_pop[i][0] /= total_value
+    # Organize elements by fitness values
+    sorted(raw_pop, key = itemgetter(0))
+    print "Before Accum: " + str(raw_pop)
+    # Accumulate element fitness values
+    for i in range(1, len(raw_pop)):
+        raw_pop[i][0] += raw_pop[i - 1][0]
+    print "After Accum: " + str(raw_pop)
+    return raw_pop
+
 # Evaluate fitness of strings
+def evaluate(puzzle, target, population):
+    ordered_list = fitness_function(puzzle, target, population)
+    
+    return ordered_list
 
 # Select strings from old population to create new population
 
@@ -90,18 +138,38 @@ def rand_string(puzzle, GP):
 puzzle_num = int(sys.argv[1])
 fd = [line for line in fi.input(sys.argv[2])]
 fi.close()
+# Runtime in seconds
 run_time = int(sys.argv[3])
+rand.seed(5)
 
-# Only for puzzle 1
+# Set target value
 T = 0
 if puzzle_num == 1:
-    T = int(fd[0])
+    T = float(fd[0])
     del fd[0]
+elif puzzle_num == 2:
+    print '2' 
+elif puzzle_num == 3:
+    print '3'
+else:
+    print 'else'
+
+# Format fd
+if puzzle_num == 1:
+    # Format layers
+    for i in range(0, len(fd)):
+        fd[i] = int(fd[i])
 
 # ======== Run Genetic Algorithm ========
-
+cur_time = 0
 start_time = time.time()
+a_list = []
+population = gen_init_pop(puzzle_num, fd, 50)
 
-# convert run_time to time.time() format
-while time.time() < run_time:
+while cur_time <= run_time:
+    # Time in seconds
+    cur_time = time.time() - start_time
+    a_list = evaluate(puzzle_num, T, population)
     # run code
+
+print a_list
