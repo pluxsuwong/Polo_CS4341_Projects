3700771f599b23918b20f58154d5432f041d8176
spirasmepulkul <spirasmepulkul@wpi.edu>
2015-09-25 14:54:21 -0400

    Revert "Revert "final push""
    This reverts commit 32eb5a718ff78ef0a53d41bdc742c24afa8ea70d.

--- Project_2/ga.py
+++ Project_2/ga.py
@@ -456,17 +456,15 @@ def mutate(children_pop, genes, temperature, puzzle, fit_num):
 
 # ==== Print Stats ====
 
-def print_stats(top_score, top_str, f_top_str, f_val, ts_gen, total_gen):
+def print_stats(top_score, top_str, f_top_str, ts_gen, total_gen):
     # best score
     print "Top Score: " + str(top_score)
     # first best string
-    print "First Top String: " + str(top_str)
-    # first best score gen
-    print "First Top Score Generation: " + str(ts_gen)
-    # final score
-    print "Final Top Score: " + str(f_val)
+    print "Top String (First): " + str(top_str)
     # final best string
-    print "Final Top String: " + str(f_top_str)
+    print "Top String (Final): " + str(f_top_str)
+    # first best score gen
+    print "Top Score Generation: " + str(ts_gen)
     # total gen
     print "Total Generations: " + str(total_gen)
 
@@ -612,32 +610,14 @@ for s in gen_snapshot:
 
 print ''
 final_string = population[0]
-final_sol = 0.0
-if puzzle_num == 1:
-    final_sol = puzzle_1_score_calc(population[0], fd, target)
-elif puzzle_num == 2:
-    final_sol = puzzle_2_score_calc(population[0], fd)
-elif puzzle_num == 3:
-    final_sol = puzzle_3_score_calc(population[0], fd)
-
-print_stats(record, record_string, final_string, final_sol, record_gen, total_gen)
-
+print_stats(record, record_string, final_string, record_gen, total_gen)
 
 # Generate csv data file
-# Create necessary directory
-prefix = "P_" + str(puzzle_num) 
-directory = prefix
-try: 
-    os.makedirs(directory)
-except OSError:
-    if not os.path.isdir(directory):
-        raise
-# Create files
 file_num = 0
-file_name = prefix + "/P_" + str(puzzle_num) + "_N_" + str(file_num)  + ".csv"
+file_name = "P_" + str(puzzle_num) + "_N_" + str(file_num)  + ".csv"
 while os.path.isfile(file_name):
     file_num += 1
-    file_name = "P_" + str(puzzle_num) + "/P_" + str(puzzle_num) + "_N_" + str(file_num)  + ".csv"
+    file_name = "P_" + str(puzzle_num) + "_N_" + str(file_num)  + ".csv"
 
 csv_file = open(file_name, 'w')
 col_0 = "Generation"
--- Project_2/run_tests.sh
+++ Project_2/run_tests.sh
@@ -1,19 +1,37 @@
 #!/bin/bash
+
+echo
 echo "Running Tests for Puzzle 1"
 python ga.py 1 file_input_1.txt 10
 python ga.py 1 file_input_1.txt 10
 python ga.py 1 file_input_1.txt 10
 python ga.py 1 file_input_1.txt 10
 python ga.py 1 file_input_1.txt 10
+if [ ! -d P_1 ]; then
+    mkdir P_1
+fi
+mv P_1_* P_1
+
+echo
 echo "Running Tests for Puzzle 2"
 python ga.py 2 file_input_2.txt 10
 python ga.py 2 file_input_2.txt 10
 python ga.py 2 file_input_2.txt 10
 python ga.py 2 file_input_2.txt 10
 python ga.py 2 file_input_2.txt 10
+if [ ! -d P_2 ]; then
+    mkdir P_2
+fi
+mv P_2_* P_2
+
+echo
 echo "Running Tests for Puzzle 3"
 python ga.py 3 file_input_3.txt 10
 python ga.py 3 file_input_3.txt 10
 python ga.py 3 file_input_3.txt 10
 python ga.py 3 file_input_3.txt 10
 python ga.py 3 file_input_3.txt 10
+if [ ! -d P_3 ]; then
+    mkdir P_3
+fi
+mv P_3_* P_3
